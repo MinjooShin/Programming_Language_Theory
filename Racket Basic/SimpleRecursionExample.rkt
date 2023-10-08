8238629b62b0c1c2fee170c7bc5a9fa67e27a6b3
MinjooShin <74174008+MinjooShin@users.noreply.github.com>
2023-10-09 00:39:35 +0900

    Add files via upload

---
+++ Racket Basic/SimpleRecursionExample.rkt
@@ -0,0 +1,11 @@
+#lang plai
+;my-length: list -> number
+;to get the length of a list
+;(test (my-length '(a b c)) 3)
+;(test (my-length empty) 0)
+(define (my-length lst)
+  (cond
+    [(empty? lst) 0]
+    [else (+ 1 (my-length(rest lst)))]))
+
+(test (my-length '(a b c)) 3)
